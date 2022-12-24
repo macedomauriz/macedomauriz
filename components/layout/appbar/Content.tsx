a7f37c295c8b3cc23ae1cdc6e00965fe21942362
Rodrigo Macedo <rodrigo1987macedo@gmail.com>
2022-12-24 18:57:28 -0300

    navbar link highlight

--- components/layout/appbar/Content.tsx
+++ components/layout/appbar/Content.tsx
@@ -5,6 +5,7 @@ import { IconProp } from "@fortawesome/fontawesome-svg-core"
 import { faGithubAlt, faLinkedinIn } from "@fortawesome/free-brands-svg-icons"
 import { ThemeSwitch } from "./content/ThemeSwitch"
 import { GetInTouch } from "./content/GetInTouch"
+import { useRouter } from "next/router"
 
 // check if a media-query hook is worth it for duplication of content
 const socialMedia = [
@@ -29,13 +30,22 @@ const navigation = [
   },
 ]
 
+function matchURL(r: any, text: string) {
+  const url = r.replace(/\//g, "")
+  return (url === "" && text === "Home") ?? url === text
+}
+
 export const DesktopContent = () => {
+  const router = useRouter()
   return (
     <>
       <Navbar.Content hideIn="xs">
         {navigation.map(i => {
           return (
-            <Navbar.Link key={i.text} isActive>
+            <Navbar.Link
+              key={i.text}
+              isActive={matchURL(router.asPath, i.text)}
+            >
               {i.text}
             </Navbar.Link>
           )
@@ -57,6 +67,7 @@ export const DesktopContent = () => {
 }
 
 export const MobileContent = () => {
+  const router = useRouter()
   const CollapseItem = styled(Navbar.CollapseItem, {
     justifyContent: "center",
     height: 50,
@@ -65,7 +76,7 @@ export const MobileContent = () => {
     <Navbar.Collapse disableAnimation>
       {navigation.map(i => {
         return (
-          <CollapseItem key={i.text} isActive>
+          <CollapseItem key={i.text} isActive={matchURL(router.asPath, i.text)}>
             {i.text}
           </CollapseItem>
         )
