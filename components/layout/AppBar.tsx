b618c8daf00a434cb0406d9bf1761c5e00955578
Rodrigo Macedo <rodrigo1987macedo@gmail.com>
2022-12-28 15:13:38 -0300

    @react-hook/media-query

--- components/layout/AppBar.tsx
+++ components/layout/AppBar.tsx
@@ -1,9 +1,13 @@
 import { useTheme, Navbar } from "@nextui-org/react"
+import { useMediaQuery } from "@react-hook/media-query"
+
 import { styled } from "@nextui-org/react"
 import { DesktopContent, MobileContent } from "./appbar/Content"
 
 export const AppBar = () => {
   const { isDark } = useTheme()
+  const isMobile = useMediaQuery("only screen and (max-width: 650px)")
+
   const BrandImage = styled("img", {
     filter: `invert(${isDark ? 0 : 1})`,
   })
@@ -13,8 +17,8 @@ export const AppBar = () => {
         <BrandImage src="/logo.svg" alt="logo" width={40} height={40} />
       </Navbar.Brand>
       <Navbar.Toggle showIn="xs" />
-      <DesktopContent />
-      <MobileContent />
+      {!isMobile && <DesktopContent />}
+      {isMobile && <MobileContent />}
     </Navbar>
   )
 }
