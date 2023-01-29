59bced95924aa510e9b5ff990f2b3eb8f1666b48
Rodrigo Macedo <rodrigo1987macedo@gmail.com>
2023-01-29 20:52:35 -0300

    ordered public assets, removed unused images

--- components/AnimationHero.tsx
+++ components/AnimationHero.tsx
@@ -3,12 +3,12 @@ import { styled, keyframes } from "@nextui-org/react"
 export function AnimationHero() {
   const sprite = keyframes({
     "0%": { backgroundPosition: "0px" },
-    "100%": { backgroundPosition: "-1400px" },
+    "100%": { backgroundPosition: "-1400px" }, // 7*200
   })
   const AnimationHeroWrapper = styled("div", {
     width: 200,
     height: 300,
-    background: "url('/standard.png')",
+    background: "url('/sprites/dark-mode.png')",
     filter: "drop-shadow(0 0 0.15rem skyblue)",
     animationName: `${sprite}`,
     animationDuration: "0.4s",
