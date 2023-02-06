672c7a1d1e0b138d81f272cfc10da21fb3e31510
Rodrigo Macedo <rodrigo1987macedo@gmail.com>
2023-02-06 17:05:08 -0300

    AppBar padding adjustment

--- components/layout/AppBar.tsx
+++ components/layout/AppBar.tsx
@@ -4,18 +4,22 @@ import { DesktopContent, MobileContent } from "./appbar/Content"
 
 export const AppBar = () => {
   const { isDark } = useTheme()
-
+  const AppBarWrapper = styled(Navbar, {
+    "> div": {
+      padding: "0 10px",
+    },
+  })
   const BrandImage = styled("img", {
     filter: `invert(${isDark ? 0.08 : 0.97})`,
   })
   return (
-    <Navbar variant="sticky" disableShadow isBordered>
+    <AppBarWrapper variant="sticky" disableShadow isBordered>
       <Navbar.Brand>
         <BrandImage src="/logo.svg" alt="logo" width={45} height={45} />
       </Navbar.Brand>
       <Navbar.Toggle showIn="xs" />
       <DesktopContent />
       <MobileContent />
-    </Navbar>
+    </AppBarWrapper>
   )
 }
