2da08660b33981488cf1e481ab52d6c2bbfcf145
Rodrigo Macedo <rodrigo1987macedo@gmail.com>
2023-02-13 08:25:15 -0300

    layout padding fix

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,7 +11,7 @@ export function Layout({ children }: LayoutProps): JSX.Element {
     <div style={{ display: "flex", justifyContent: "center" }}>
       <div style={{ maxWidth: 1200 }}>
         <AppBar />
-        <Container as="main" gap={1}>
+        <Container as="main" gap={2}>
           {children}
         </Container>
         <Spacer y={1} />
