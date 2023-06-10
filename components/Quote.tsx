043383256cbefb4b2ccd5a5fa37293644c18fa41
Rodrigo Macedo <rodrigo1987macedo@gmail.com>
2023-06-10 20:31:02 -0300

    updated dependencies

--- components/Quote.tsx
+++ components/Quote.tsx
@@ -1,50 +1,26 @@
 import { styled } from "@nextui-org/react"
 import Balancer from "react-wrap-balancer"
-// import { useTheme } from "@nextui-org/react"
-// import CustomLink from "./CustomLink"
-// import Image from "next/image"
 import { Typography } from "./Typography"
-// import { useState } from "react"
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-// import {
-//   faCircleArrowRight,
-//   faCircleArrowLeft,
-// } from "@fortawesome/free-solid-svg-icons"
-
-// interface CustomersProps {
-//   src: string
-//   alt: string
-//   href: string
-//   width: number
-//   height: number
-//   description: string
-//   translateY?: string
-// }
-
-// const customers: CustomersProps[] = []
 
 export function Quote(): JSX.Element {
-  // const { isDark } = useTheme()
-  // const [visible, setVisible] = useState(false)
-
-  const CustomersWrapper = styled("div", {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
+  const QuoteWrapper = styled("div", {
     textAlign: "center",
+    "> h3:first-child": {
+      fontStyle: "italic",
+    },
   })
 
   return (
-    <CustomersWrapper>
-      <Typography h2 paragraph color="$gray800" css={{ fontStyle: "italic" }}>
+    <QuoteWrapper>
+      <Typography h3 paragraph color="$gray800">
         <Balancer>
           "When things are simple, fewer mistakes are made. The most expensive
           part of a building is the mistakes."
         </Balancer>
       </Typography>
-      <Typography color="$gray800">
+      <Typography color="$gray700">
         Ken Follett - The Pillars of the Earth
       </Typography>
-    </CustomersWrapper>
+    </QuoteWrapper>
   )
 }
