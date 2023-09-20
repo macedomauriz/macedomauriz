e082d975c5edcea787fc79229c282cf7076274d1
Rodrigo Macedo <rodrigo1987macedo@gmail.com>
2023-09-20 18:49:55 -0300

    quick show push

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,28 @@
 import { Spacer } from "@nextui-org/react"
-import { GetStaticProps } from "next"
-import { getAllPosts, PostProps } from "utils/mdx"
-import { Customers } from "../components/Customers"
+// import { GetStaticProps } from "next"
+// import { getAllPosts, PostProps } from "utils/mdx"
+// import { Customers } from "../components/Customers"
 import { Hero } from "../components/Hero"
-import { LatestBlogPosts } from "../components/LatestBlogPosts"
+// import { LatestBlogPosts } from "../components/LatestBlogPosts"
 import { Quote } from "../components/Quote"
-import Image from "next/image"
+// import Image from "next/image"
 
-interface HomeProps {
-  posts: PostProps[]
-}
+// interface HomeProps {
+// posts: PostProps[]
+// }
 
-export default function Home({ posts }: HomeProps) {
+export default function Home() {
   return (
     <>
       <Hero />
       <Spacer y={5} />
       <Quote />
-      <div
-        style={{
-          position: "relative",
-          width: "100%",
-          height: "30vw",
-          background: "red",
-        }}
-      >
-        <Image
-          src="/mario.png"
-          alt="example"
-          fill
-          style={{
-            objectFit: "contain",
-            maxWidth: 100,
-            background: "blue",
-          }}
-        />
-      </div>
-      <LatestBlogPosts posts={posts} />
       <Spacer y={5} />
-      <Customers />
+      <div style={{ textAlign: "center" }}>IN CONSTRUCTION...</div>
     </>
   )
 }
 
-export const getStaticProps: GetStaticProps = async () => {
-  return { props: { posts: getAllPosts() } }
-}
+// const getStaticProps: GetStaticProps = async () => {
+//   return { props: { posts: getAllPosts() } }
+// }
