1985f6ed8a3f8a98282ed2ed771cff5ec8262920
Rodrigo Macedo <rodrigo1987macedo@gmail.com>
2023-11-01 13:07:02 -0300

    Lambda update Github actions

--- aws-lambda-ses/aws-lambda-ses.py
+++ aws-lambda-ses/aws-lambda-ses.py
@@ -5,13 +5,14 @@ import json
 def lambda_handler(event, context):
     client = boto3.client("ses")
     event_body = json.loads(event["body"])
+    my_email = "rodrigo@macedomauriz.com"
 
     subject = event_body["subject"]
     body = event_body["body"]
+    email = event_body["email"]
+
     message = {"Subject": {"Data": subject}, "Body": {"Html": {"Data": body}}}
     response = client.send_email(
-        Source="rodrigo@macedomauriz.com",
-        Destination={"ToAddresses": ["rodrigo1987macedo@gmail.com"]},
-        Message=message,
+        Source=my_email, Destination={"ToAddresses": [my_email, email]}, Message=message
     )
     return response
