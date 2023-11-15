b9cb512b3587a0dd99ba885ae9adb2bf21b7628e
Rodrigo Macedo <rodrigo1987macedo@gmail.com>
2023-11-15 04:49:50 -0300

    CallOuts go with defined theme

--- components/PostCallOut.tsx
+++ components/PostCallOut.tsx
@@ -1,27 +1,35 @@
 import { styled, useTheme } from "@nextui-org/react"
-// import { useTheme as useNextTheme } from "next-themes"
+import { useTheme as useNextTheme } from "next-themes"
 
 interface PostCallOutProps {
   children: React.ReactNode
   type: "warning" | "note"
 }
 
+const getBackgroundColor = (type: PostCallOutProps["type"], theme: any) => {
+  if (type == "warning") {
+    return theme.colors.yellow200.value
+  }
+  return theme.colors.blue200.value
+}
+
+const getBorderLeftColor = (type: PostCallOutProps["type"], theme: any) => {
+  if (type == "warning") {
+    return theme.colors.yellow800.value
+  }
+  return theme.colors.blue800.value
+}
+
 export function PostCallOut({ children, type }: PostCallOutProps) {
   const { theme } = useTheme()
-  // const { resolvedTheme } = useNextTheme()
+  const { resolvedTheme } = useNextTheme()
 
   const PostCallOutWrapper = styled("div", {
-    background:
-      type === "warning"
-        ? theme?.colors.yellow200.value
-        : theme?.colors.blue200.value,
+    background: resolvedTheme && getBackgroundColor(type, theme),
     borderRadius: "0 12px 12px 0",
     padding: "2% 4%",
     margin: "24px 0",
-    borderLeft:
-      type === "warning"
-        ? `4px solid ${theme?.colors.yellow800.value}`
-        : `4px solid ${theme?.colors.blue800.value}`,
+    borderLeft: resolvedTheme && `4px solid ${getBorderLeftColor(type, theme)}`,
   })
 
   return <PostCallOutWrapper>{children}</PostCallOutWrapper>
